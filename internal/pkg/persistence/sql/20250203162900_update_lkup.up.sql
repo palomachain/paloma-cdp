e224f56b7fd8a96276a161bf89aad39aee91a035
Christian Lohr <cl@bytewerk.io>
2025-03-03 17:33:48 +0100

    chore: update exchange lookup

---
+++ internal/pkg/persistence/sql/20250203162900_update_lkup.up.sql
@@ -0,0 +1,2 @@
+UPDATE exchange_lkup SET deleted_at=now() WHERE exchange_id = 1;
+INSERT INTO exchange_lkup (address, exchange_id) VALUES ('paloma1gezx7k0dqrsuuzv62kx7rm4teylwvqqc4u6t83r3grs4sgfr8crsg6cehw', 1);
